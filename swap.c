84da789afe046b7bdcf4c1e91a259ff95a9591a1
shreya-1771 <84712068+shreya-1771@users.noreply.github.com>
2021-06-29 19:08:40 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,1 +1,26 @@
+#include<stdio.h>
+#include<cs50.h>
+#include<string.h>
 
+int swap_max(int arr[], int l, int n){
+  int max=arr[n];
+  int pos=n;
+  for(int i=n;i<l;i++){
+    if(arr[i]>max){
+      max=arr[i];
+      pos=i;
+    }
+  }
+  arr[pos]=arr[n];
+  arr[n]=max;
+}
+
+int ssort(int arr, int l){
+  for(int i=0; i<l; i++){
+    swap_max(arr, l, i);
+  }
+  printf("The sorted array is \n|");
+  for(int i=0; i<9;i++)
+    printf("%i |", arr[i]);
+  printf("\n");
+}
